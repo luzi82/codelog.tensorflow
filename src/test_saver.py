e813595f805a190e6a79b3beed72c5ec05ad753d
Luzi82 <luzi82@gmail.com>
2016-07-10 17:42:47 +0800

    more test on graph reset

--- src/test_saver.py
+++ src/test_saver.py
@@ -45,7 +45,9 @@ sess.close()
 sess = None
 saver = None
 
-for _ in range(1000):
+print('NAPJNHPV tf.Graph().as_default() test')
+
+for _ in range(100):
     t0 = int(time.time()*1000)
 
     with tf.Graph().as_default(): # without this, the restore time will become very slow in loop
@@ -58,6 +60,30 @@ for _ in range(1000):
     #         print(var0.eval(sess)) # [86.,11.,28.]
             saver = None
             t2 = int(time.time()*1000)
+
+    t3 = int(time.time()*1000)
+
+    print("{},{},{}".format(t1-t0,t2-t1,t3-t2))
+
+print('TKCPICEE tf.reset_default_graph() test')
+
+for _ in range(100):
+    t0 = int(time.time()*1000)
+    
+    tf.reset_default_graph()
+
+    var0, inData, train0 = create_train()
+    
+    sess = tf.Session()
+    saver = tf.train.Saver({'var0':var0})
+
+    t1 = int(time.time()*1000)
+    saver.restore(sess,"/tmp/FZZACAGZOU")
+    saver = None
+    t2 = int(time.time()*1000)
+    
+    sess.close()
+    sess = None
 
     t3 = int(time.time()*1000)
 
